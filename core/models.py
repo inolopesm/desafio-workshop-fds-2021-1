0e14103494fabd571e3b71cfd76589dc84c28da7
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 22:11:35 -0300

    cria model Product em core

--- core/models.py
+++ core/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.DecimalField(max_digits=9, decimal_places=2)
+    amount = models.PositiveIntegerField()
