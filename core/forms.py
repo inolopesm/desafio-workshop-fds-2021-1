5746c9e1fcc03466504a382bdd7b886d8f65e9ef
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 23:02:55 -0300

    cria ProductForm

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 from django.utils.translation import ugettext_lazy as _
-from .models import Category
+from .models import Category, Product
 
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = ['name']
         labels = {'name': _('Nome')}
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'value', 'amount', 'category']
+        labels = {'name': _('Nome'), 'value': _('Valor'), 'amount': _('Quantidade'), 'category': _('Categoria')}
