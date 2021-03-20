ca1128dd94531f9b112a5daf4f07e9a0f12f81c2
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 22:15:53 -0300

    adiciona category em Product em core

--- core/models.py
+++ core/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     value = models.DecimalField(max_digits=9, decimal_places=2)
     amount = models.PositiveIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
