e5adf8e5b649569295b88adba2ee44ef81319edd
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 23:03:34 -0300

    cria views de CRUD para Product

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import CreateView, ListView, UpdateView, DetailView
 from django.urls import reverse_lazy
-from .models import Category
-from .forms import CategoryForm
+from .models import Category, Product
+from .forms import CategoryForm, ProductForm
 
 class CategoryCreateView(CreateView):
     form_class = CategoryForm
@@ -18,3 +18,19 @@ class CategoryUpdateView(UpdateView):
 
 class CategoryDetailView(DetailView):
     model = Category
+
+class ProductCreateView(CreateView):
+    form_class = ProductForm
+    template_name = 'core/product_form.html'
+    success_url = reverse_lazy('product-list')
+
+class ProductListView(ListView):
+    model = Product
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('product-list')
+
+class ProductDetailView(DetailView):
+    model = Product
