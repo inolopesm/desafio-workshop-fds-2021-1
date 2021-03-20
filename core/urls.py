29ef06a20e39056fa79d5046253322911700f5bf
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 21:46:38 -0300

    adiciona path para CategoryListView em core

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoryCreateView
+from .views import CategoryCreateView, CategoryListView
 
 urlpatterns = [
     path('categories/create/', CategoryCreateView.as_view()),
+    path('categories/', CategoryListView.as_view()),
 ]
