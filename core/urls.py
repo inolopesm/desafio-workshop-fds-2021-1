67a56e6a5ff98bca07026e2722adc33a4e0a1f8c
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 21:59:06 -0300

    adiciona funcionalidade de editar Category

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CategoryCreateView, CategoryListView
+from .views import CategoryCreateView, CategoryListView, CategoryUpdateView
 
 urlpatterns = [
     path('categories/create/', CategoryCreateView.as_view(), name='category-create'),
     path('categories/', CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/edit', CategoryUpdateView.as_view(), name='category-update'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
 from django.urls import reverse_lazy
 from .models import Category
 from .forms import CategoryForm
@@ -6,7 +6,13 @@ from .forms import CategoryForm
 class CategoryCreateView(CreateView):
     form_class = CategoryForm
     template_name = 'core/category_form.html'
-    success_url = reverse_lazy('category_list')
+    success_url = reverse_lazy('category-list')
 
 class CategoryListView(ListView):
     model = Category
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'core/category_form.html'
+    success_url = reverse_lazy('category-list')
