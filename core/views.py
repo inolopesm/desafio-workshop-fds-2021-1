5557081b94837a5d1251dde28bc690ffa413adb2
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-19 21:46:17 -0300

    cria CategoryListView em core

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+from .models import Category
 from .forms import CategoryForm
 
 class CategoryCreateView(CreateView):
     form_class = CategoryForm
     template_name = 'core/category_form.html'
     success_url = '/categories/'
+
+class CategoryListView(ListView):
+    model = Category
