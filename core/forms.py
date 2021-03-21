1dfa4071267d94a93b7841fbb07be0eb3aafe7a9
Matheus Inocêncio Lopes <inolopesm@gmail.com>
2021-03-20 23:06:02 -0300

    adiciona validação em ProductForm

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from django.utils.translation import ugettext_lazy as _
 from .models import Category, Product
 
@@ -9,7 +9,25 @@ class CategoryForm(ModelForm):
         labels = {'name': _('Nome')}
 
 class ProductForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['value'].widget.attrs['min'] = 0
+        self.fields['value'].widget.attrs['step'] = 0.01
+        self.fields['amount'].widget.attrs['min'] = 0
+
     class Meta:
         model = Product
         fields = ['name', 'value', 'amount', 'category']
         labels = {'name': _('Nome'), 'value': _('Valor'), 'amount': _('Quantidade'), 'category': _('Categoria')}
+
+    def clean_value(self):
+        value = self.cleaned_data['value']
+        if value < 0:
+            raise ValidationError('Valor não pode ser menor que 0')
+        return value
+
+    def clean_amount(self):
+        amount = self.cleaned_data['amount']
+        if amount < 0:
+            raise ValidationError('Quantidade não pode ser menor que 0')
+        return amount
